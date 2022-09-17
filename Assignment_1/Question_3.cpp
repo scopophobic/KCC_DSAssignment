4d60d1cc3a1581150690c018ad64e95818220865
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-09-17 23:34:49 +0530

    Create Question_3.cpp

---
+++ Assignment_1/Question_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n";
+    cout << fixed << setprecision(2) << d<<endl;
+    cout << fixed << setprecision(2) << e<<endl;
+    
+    return 0;
+}
