d70dbb3c21ac3fa04bde86214403955af0ba672e
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-10-09 19:29:18 +0530

    Update append-and-delete.cpp

--- Assignment_4/append-and-delete.cpp
+++ Assignment_4/append-and-delete.cpp
@@ -2,24 +2,76 @@
 
 using namespace std;
 
-int main(){
-    string s;
-    cin >> s;
-    string t;
-    cin >> t;
-    int k;
-    cin >> k;
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+string appendAndDelete(string s, string t, int k) {
     int cl=0;
     while(cl<s.size() && cl<t.size()){
         if(s[cl]!=t[cl]) break;
         cl++;
     }
     if(s.size()-cl+t.size()-cl<=k&& (s.size()-cl+t.size()-cl)%2==k%2){
-        cout<<"Yes"<<endl;
+        return "Yes";
     }
     else if(s.size()+t.size()<=k){
-        cout<<"Yes"<<endl;
+        return "Yes";
     }
-    else cout<<"No"<<endl;
+    else return "No";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string t;
+    getline(cin, t);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = appendAndDelete(s, t, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
