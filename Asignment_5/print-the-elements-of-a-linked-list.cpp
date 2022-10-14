b8ce746a46697b5f58b58ecf1c11e1b3dcb0ac01
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-10-15 01:15:50 +0530

    Create print-the-elements-of-a-linked-list.cpp

---
+++ Asignment_5/print-the-elements-of-a-linked-list.cpp
@@ -0,0 +1,9 @@
+
+
+void printLinkedList(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
