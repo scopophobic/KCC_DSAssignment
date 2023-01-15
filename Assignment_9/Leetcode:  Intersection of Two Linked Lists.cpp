52b31f64a6dbcb78e9f2baba0199b13a5b7e1742
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-01-16 04:17:06 +0530

    Create Leetcode:  Intersection of Two Linked Lists.cpp

---
+++ Assignment_9/Leetcode:  Intersection of Two Linked Lists.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+      
+   
+        set<ListNode * > st;
+      
+        while(headA)
+        {
+            st.insert(headA);
+            headA=headA->next;
+        }
+        while(headB)
+        {
+            if (st.find(headB)!=st.end()) return headB; 
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
